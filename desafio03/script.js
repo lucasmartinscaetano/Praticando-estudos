fefdef9875b85a1a947021d4104300c449ef187b
lucasmartinscaetano <108697152+lucasmartinscaetano@users.noreply.github.com>
2022-08-06 18:50:48 -0300

    desafios att

---
+++ desafio03/script.js
@@ -0,0 +1,7 @@
+var vaisehospedar = window.confirm('Seja bem-vindo(a), aventureiro(a)! Gostaria de pagar R$50,00 para passar a noite em nossa hospedagem?')
+
+if(vaisehospedar){
+    window.alert('Ótimo! nós temos as melhores camas da região')
+}else{
+    window.alert('Que pena! Achei que você fosse mais legal.')
+}
---
+++ desafio04/script.js
@@ -0,0 +1,4 @@
+var num = 0
+for(num; num <= 10; num++){
+    console.log(`O número atual é o ${num}`)
+}
