daf54eae50bf30e2c4e7b5c1a3e9556b87434f1c
lucasmartinscaetano <108697152+lucasmartinscaetano@users.noreply.github.com>
2022-08-06 17:49:32 -0300

    desafio

---
+++ desafio01/script.js
@@ -0,0 +1,7 @@
+
+function clicar(){
+    var caixa = document.querySelector("input#caixa")
+    var res = document.querySelector('div#res') 
+    window.alert(`Seja bem-vindo, ${caixa.value}`)
+   
+}
